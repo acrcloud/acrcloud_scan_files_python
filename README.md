929cb3846c96c638363aa8caa59dbb97e07bb2ad
owlwang <owlwang@outlook.com>
2016-05-06 18:37:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# ACRCloud Scan Files Tool
+
+## Requirements
+
+- Python 2.7
+- backports.csv==1.0.1
+- eyeD3==0.7.9
+- requests==2.10.0
+## Install
+ 
+ If your system is windows you must to download Python 2,and install it.
+ 
+ Open your Terminal then cd to the script directory.
+ 
+ ```
+    source venv/bin/activate
+ ```
+ Or you can run 
+ 
+ ```
+    pip install requirements.txt
+ ```
+
+## Usage: 
+ 
+ Before you use this script,you must have acrcloud host,access_key and access_secret.
+ If you haven't have these ,you can register one https://console.acrcloud.com/signup
+ 
+ Change the content of config.json,fill in your host,access_key and access_secret
+ ```
+{
+  "host": "ap-southeast-1.api.acrcloud.com",
+  "access_key": "xxxxx",
+  "access_secret": "xxxxx"
+}
+ ```
+
+ ```python
+    python recognize.py path
+ ```
+  Example:
+ ```python
+    python acrcloud_scan_files_python.py ~/music
+ ```
+
+Default is scan folder where this script in.
+
+The results are saved in the folder where this script in.
---
+++ acrcloud_scan_files_python.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# encoding:utf-8
+
+import os
+import codecs
+import sys
+import json
+from acrcloudpysdk.recognizer import ACRCloudRecognizer
+import time
+import eyed3
+from backports import csv
+from multiprocessing import Pool, cpu_count
+
+
+step = 10
+
+
+def get_tracks_artists(artists):
+    artists_namelist = []
+    for artist in artists:
+        artists_namelist.append(artist['name'])
+    space = ','
+    artists_names = space.join(artists_namelist)
+    return artists_names
+
+
+def recognize_music(filename):
+    mp3_file_data = eyed3.load(filename)
+    duration = mp3_file_data.info.time_secs
+    with codecs.open('config.json', 'r') as f:
+        json_config = json.loads(f.read())
+        host = json_config['host']
+        key = json_config['access_key']
+        secret = json_config['access_secret']
+
+    config = {
+        'host': str(host),
+        'access_key': str(key),
+        'access_secret': str(secret),
+        'debug': False,
+        'timeout': 10  # seconds
+    }
+    re = ACRCloudRecognizer(config)
+    result = []
+    for i in range(0, duration, step):
+        current_time = time.strftime('%H:%M:%S', time.gmtime(i))
+        res_data = re.recognize_by_file(filename, i)
+        try:
+            ret_dict = json.loads(res_data)
+        except:
+            ret_dict['status']['msg'] = 'Error'
+        if ret_dict['status']['msg'] == 'Success':
+            metadata = ret_dict['metadata']
+            try:
+                title = metadata['music'][0]['title']
+            except:
+                title = ''
+            try:
+                isrc = metadata['music'][0]['external_ids']['isrc']
+            except:
+                isrc = ''
+            try:
+                acrid = metadata['music'][0]['acrid']
+            except:
+                acrid = ''
+            try:
+                label = metadata['music'][0]['label']
+            except:
+                label = ''
+            try:
+                album = metadata['music'][0]['album']['name']
+            except:
+                album = ''
+            try:
+                artists = get_tracks_artists(metadata['music'][0]['artists'])
+            except:
+                artists = ''
+            try:
+                dezzer = str(metadata['music'][0]['external_metadata']['deezer']['track']['id'])
+            except:
+                dezzer = ''
+            try:
+                spotify = str(metadata['music'][0]['external_metadata']['spotify']['track']['id'])
+            except:
+                spotify = ''
+            try:
+                itunes = str(metadata['music'][0]['external_metadata']['itunes']['track']['id'])
+            except:
+                itunes = ''
+            try:
+                custom_files_title = metadata['custom_files'][0]['title']
+            except:
+                custom_files_title = ''
+            try:
+                audio_id = metadata['custom_files'][0]['audio_id']
+            except:
+                audio_id = ''
+            res = (current_time, title, artists, album,
+                   acrid, label, isrc, dezzer, spotify,
+                   itunes, custom_files_title, audio_id)
+            result.append(res)
+
+        i += step
+
+    return result
+
+
+def save_results(target):
+    try:
+        results = recognize_music(target)
+        print results
+        filename = 'result-' + target.split('/')[-1].strip() + '.csv'
+        try:
+            os.remove(filename)
+        except:
+            pass
+        with codecs.open(filename, 'w', 'utf-8-sig') as f:
+            fields = ['time', 'title', 'artists', 'album', 'acrid', 'isrc', 'dezzer', 'spotify', 'itunes']
+            dw = csv.writer(f)
+            dw.writerow(fields)
+            dw.writerows(results)
+    except:
+        pass
+
+
+def main():
+    p = Pool(cpu_count())
+    path = os.getcwd()
+    file_list = os.listdir(path)
+    p.map(save_results, file_list)
+    p.close()
+    p.join()
+
+if __name__ == '__main__':
+    if 1 != 1:
+        print '[-] Usage: python recognize.py'
+        print '[-] Put this python script to the file you want to recognize'
+        sys.exit()
+    else:
+        main()
