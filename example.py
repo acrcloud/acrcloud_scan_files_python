03b2838c7a80453da6e23357223835c791a05457
ICYMYM <573230586@qq.com>
2018-11-12 18:17:52 +0800

    update scan file

--- example.py
+++ example.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import json
 from acrcloud_scan_files_libary import ACRCloud_Scan_Files
 
 if __name__ == "__main__":
@@ -33,26 +34,28 @@ if __name__ == "__main__":
     #export dir
     export_dir = "./"
     #export to csv
-    export_filename_csv = "test.csv"
+    export_filename_csv = filepath + ".csv"
     acr_sfile.export_to_csv(result_list, export_filename_csv, export_dir)
     #export to xlsx
-    export_filename_xlsx = "test.xlsx"
+    export_filename_xlsx = filepath + ".xlsx"
     acr_sfile.export_to_xlsx(result_list, export_filename_xlsx, export_dir)
 
     #iterator to get the result of each fragment
     result_list2 = []
-    for item in acr_sfile.for_recognize_file(filepath, start_time, stop_time, step, rec_length):
-        result_list2.append(item)
-        filename = item["file"]
-        timestamp = item["timestamp"]
-        res = acr_sfile.parse_data(item["result"])
-        title = res[3]
-        print filename, timestamp, title
+    with open(filepath+"_raw_result.lst", "wb") as wfile:
+        for item in acr_sfile.for_recognize_file(filepath, start_time, stop_time, step, rec_length):
+            result_list2.append(item)
+            filename = item["file"]
+            timestamp = item["timestamp"]
+            res = acr_sfile.parse_data(item["result"])
+            title = res[3]
+            print filename, timestamp, title
+            wfile.write("{0}\n".format(json.dumps(item)))
 
     #get results with played-duration
     filter_results = acr_sfile.apply_filter(result_list2)
     #export the results to xlsx
-    export_filtername_xlsx = "test_with_duration.xlsx"
+    export_filtername_xlsx = filepath + "_with_duration.xlsx"
     acr_sfile.export_to_xlsx(filter_results, export_filtername_xlsx, export_dir)
 
 
